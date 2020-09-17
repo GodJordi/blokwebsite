106fc88378b9edfaebe92e5b6c821ffc095739f6
GodJordi <jordi888@live.nl>
2020-09-17 12:20:31 +0200

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,63 +1,17 @@
-// JavaScript Document
-const toggle = document.querySelector(".toggle");
-const menu = document.querySelector(".menu");
- 
+/* global document */
+var toggle = document.querySelector(".toggle");
+var menu = document.querySelector(".menu");
+
 /* Toggle mobile menu */
 function toggleMenu() {
-    if (menu.classList.contains("active")) {
-        menu.classList.remove("active");
-         
-        // adds the menu (hamburger) icon
-        toggle.querySelector("a").innerHTML = "<i class=’fas fa-bars’></i>";
-    } else {
-        menu.classList.add("active");
-         
-        // adds the close (x) icon
-        toggle.querySelector("a").innerHTML = "<i class=’fas fa-times’></i>";
-    }
-}
- 
-/* Event Listener */
-toggle.addEventListener("click", toggleMenu, false);
-
-const items = document.querySelectorAll(".item");
- 
-
-
-
-
-/* Activate Submenu */
-function toggleItem() {
-  if (this.classList.contains("submenu-active")) {
-    this.classList.remove("submenu-active");
-  } else if (menu.querySelector(".submenu-active")) {
-    menu.querySelector(".submenu-active").classList.remove("submenu-active");
-    this.classList.add("submenu-active");
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
   } else {
-    this.classList.add("submenu-active");
+    menu.classList.add("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
   }
 }
- 
+
 /* Event Listeners */
-for (let item of items) {
-    if (item.querySelector(".submenu")) {
-      item.addEventListener("click", toggleItem, false);
-      item.addEventListener("keypress", toggleItem, false);
-    }   
-}
-
-
-
-
-
-/* Close Submenu From Anywhere */
-function closeSubmenu(e) {
-  let isClickInside = menu.contains(e.target);
- 
-  if (!isClickInside && menu.querySelector(".submenu-active")) {
-    menu.querySelector(".submenu-active").classList.remove("submenu-active");
-  }
-}
- 
-/* Event listener */
-document.addEventListener("click", closeSubmenu, false);
+toggle.addEventListener("click", toggleMenu, false);
